1feef9d8e560dc37ec0ddaa7d9101780d9197842
Extoren <sffjo.aarre@gmail.com>
2023-08-21 19:33:20 +0200

    info update

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = 'fbfe54c431b28e24063d27c487c54d1d'  # Replace with your actual secret key
+app.secret_key = 'fbfe54c431b28e24063d27c487c54d1d'  # Replace with your actual secret key, not with mine please
 
 # Simulated database of usernames and passwords
 users = {
     "Extoren": "password1",
 }
 
+# You know the rest
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -25,6 +26,13 @@ def login():
             return "Login failed"
     return render_template("login.html")
 
+
+# Make all the info .html work
+@app.route("/info/<page>")
+def info_page(page):
+    return render_template("info/" + page)
+
+# Make the login .html work
 @app.route("/logout")
 def logout():
     session.pop('logged_in', None)
